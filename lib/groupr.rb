a856cac823040984c67de71a573ecba9b4460a06
Nikky Southerland <nikky@uw.edu>
2012-12-06 14:46:35 -0800

    Initial support for group_exists? and view_group

--- lib/groupr.rb
+++ lib/groupr.rb
@@ -1,2 +1,79 @@
+require 'net/http'
+require 'nokogiri'
 class Groupr
+	attr_accessor :api_url, :certificate, :key, :uw_ca_file
+	def initialize
+		@api_url = "https://iam-ws.u.washington.edu:7443/group_sws/v2"
+		@uw_ca_file = "#{ENV['HOME']}/uwca.crt"
+	end
+	def make_get_request
+		options = {
+			use_ssl: true,
+			cert: OpenSSL::X509::Certificate.new(@certificate),
+			key: OpenSSL::PKey::RSA.new(@key),
+			ca_file: @uw_ca_file,
+			verify_mode: OpenSSL::SSL::VERIFY_PEER
+		}
+		Net::HTTP.start(@uri.host, @uri.port, options) do |http|
+			request = Net::HTTP::Get.new(@uri.request_uri)
+			@response = http.request(request)
+		end
+		validate_response(@response)
+	end
+	# This method returns nil if the http request doesn't return a 200. Maybe also support for not-authorized?
+	def validate_response(response)
+		nil if response.code.to_i != 200
+		response.body if response.code.to_i == 200
+	end
+	def group_exists?(group)
+		@uri = URI.parse("#{@api_url}/group/#{group}")
+		make_get_request
+		if @response.code.to_i == 200
+			true
+		else
+			false
+		end
+	end
+	
+
+
+	def view_group(group)
+		@uri = URI.parse("#{@api_url}/group/#{group}")
+		make_get_request
+		@doc = Nokogiri::HTML(@response.body)
+		{
+			title: get_title,
+			description: get_description,
+			name: get_name,
+			regid: get_regid,
+			contact: get_contact
+		}
+	end
+
+
+
+	def get_contact
+		@doc.xpath('//span[@class="contact"]').text
+	end
+
+	def get_title
+		@doc.xpath('//span[@class="title"]').text
+	end
+	def get_description
+		@doc.xpath('//span[@class="description"]').text
+	end
+	def get_name
+		@doc.xpath('//span[@class="name"]').text
+	end
+	def get_regid
+		@doc.xpath('//span[@class="regid"]').text
+	end
+
+
+
+	# def other thing to use
+	# http.ssl_timeout
+	# It throws TimeoutError exceptions for timeouts
+	# http.cert_store to for.... the CA?
+	# or maybe http.ca_file
 end
--- spec/groupr_spec.rb
+++ spec/groupr_spec.rb
@@ -1,8 +1,61 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Groupr" do
-	it "Instantiates a new instance of groupr" do
+
+	before do
 		@group = Groupr.new
+		@group.certificate = File.read("#{ENV['HOME']}/nikky_cac_washington_edu.cert")
+		@group.key = File.read("#{ENV['HOME']}/nikky_cac_washington_edu.key")
 	end
-	it "Loads a certificate"
+
+	it "Checks for network connectivity"
+	it "Should allow you to specify a custom Service API" do
+		@group.respond_to?(:api_url).should eq true
+	end
+	it "Should allow you to specify your certificate" do
+		@group.respond_to?(:certificate).should eq true
+	end
+	it "Should Allow you to specify your key" do
+		@group.respond_to?(:key).should eq true
+	end
+
+	context "#groupexists?" do
+		it "Should tell you that the uw_employees group exists" do
+			@group.group_exists?("u_nikky_git").should be_true
+		end
+		it "Should tell you that the a_team group does not exist" do
+			@group.group_exists?("a_team").should be_false
+		end
+	end
+
+	context "#view_group" do
+		before do
+			@results = @group.view_group("u_nikky_git")
+		end
+		it "Should tell you the group name" do
+			@results[:name].should eq "u_nikky_git"
+		end
+		it "Should tell you the group description" do
+			@results[:description].should_not == ""
+		end
+		it "Should tell you the group contact" do
+			@results[:contact].should eq "nikky"
+		end
+		it "Should tell you the group title" do
+			@results[:title].should eq "nikky git"
+		end
+	end
+
+
+	context "#get_members" do
+		it "Gets the members of a group"
+	end
+
+	context "#add_member" do
+		it "Adds a member to a group"
+	end
+
+	context "#remove_member" do
+		it "Removes a member from a group"
+	end
 end
